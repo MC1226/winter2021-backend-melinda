0ce853299a1ce44ffb7ce62207c6b4426bed5077
Melinda <melindacass1226@gmail.com>
2021-11-22 03:28:06 -0800

    update

--- lab/week3/web_server/index.js
+++ lab/week3/web_server/index.js
@@ -1,41 +1,45 @@
-// Use the express package by using const express.
+// Use the Express package.
 const express = require("express");
 
-// Use the body-parser package.
+// Use the body-parser package. Deprecated, no longer used as body-parser is accessed through Express.js.
 const bodyParser = require("body-parser");
 
-// Creates an Express oject from the Express package.
+// Creates an Express object from the Express package.
 const app = express();
 
-// Allows Express to use th HTTP module of Node.
+// Allows Express to use the HTTP module of Node.
 const http = require("http").Server(app);
 
-// Necessary settings for body-parser, which automatically detects and converts JSON in out HTTP request.
+// Necessary settings for body-parser, which automatically detects and converts JSON in our HTTP requests.
 app.use(express.json({strict: false}));
-
 app.use(express.urlencoded({extended: true}));
 
 const port = 3000;
 
-// In order to finish creating a server you have to tell it what port to listen to.
-// Tells Express/HTTP module what port to listen to. The most common port to use is 3000.
-http.listen(port); // Common Development Ports: 8080, 3000 for Node.
+// Tells Express/HTTP what port to listen to.
+http.listen(port); // Common Development Ports: 8080, 3000 = Node.
 
 console.log("Express server is running on port " + port + ".");
 
 // Express Routes. Allows us to "redirect" a URL filepath to a specific folder.
 app.use("/", express.static("public_html/"));
 
-// This .post() method handles any POST requests made by the / test filepath. When a request is made, run the callback function.
-app.post("/test", function(request, response){
-    
-    // Data from the front end is located inside of request.body;
-    
+// This .post() method handles any POST requests made by the /test filepath. When a request is made, run the callback function.
+app.post("/test", function (request, response) {
+    // Data from the front end is located inside of request.body.
 
-    // The following code only runs when a request is made.
-    console.log("The front end sends the following:", request.body);
+    // The following code runs when a request is made.
+    console.log("The front end sends the following: ", request.body);
 
-    // We send a response to the front-end client with a Status Code of 200 (which means ok).
-    response.sendStatus(200);
+    let myResponseObject = {
+        response: "Heard you loud and clear!"
+    };
 
+    // We send a response to the front-end client with a Status Code of 200 (which means OK).
+    // response.sendStatus(200);
+
+    // Send an JavaScript object (which will be automatically converted to a JSON string) to the front-end. This also ends the .post() method, similar to the return keyword.
+    response.send(myResponseObject);
 });
+
+
